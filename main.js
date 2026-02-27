3e94b78eabfad3db5ce316885e93896950fd2572
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-02-27 16:06:47 +0500

    boshlandi

---
+++ main.js
@@ -0,0 +1,35 @@
+// let a = +prompt("a soni kiriting")
+// function check(a){
+//       if (a > 0) {
+//         alert("musbat son")
+//       } else if(a === 0){
+//         alert("a nolga teng")
+//       }else{
+//         alert("nodan kichkina")
+//       }
+// }
+// check(a)
+
+// let a = +prompt("a sonni kiritng")
+// let b = +prompt('b sonnu kiritng')
+// let c = +prompt("c sonni kiritng")
+
+// function check(a, b, c) {
+//   katta=Math.max(a, b, c);
+//   kichik=Math.min(a, b, c);
+//   orta=(a + b + c)-(katta + kichik)
+//   console.log(orta);
+// }
+// check(a, b, c)
+
+// let a = prompt('ism kiriting')
+
+// function checkName(a) {
+//   if (a && 5) {
+//     alert("ism katta")    
+//   } else {
+//     alert('ism kichkina')
+//   }  
+//   console.log(a && 5);
+// }
+// checkName(a)  
